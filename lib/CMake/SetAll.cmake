cbaa6321c906f47db851665a7544bf2e4f7c7629
Jonah Bernhard <jeb65@grads-ad>
2013-05-24 16:08:11 -0400

    Add option for quiet build.

--- lib/CMake/SetAll.cmake
+++ lib/CMake/SetAll.cmake
@@ -5,12 +5,18 @@
 # settings for C++
 macro (SetCXX)
   find_package(GSL REQUIRED)
+  if (QUIET)
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-write-strings")
+  endif (QUIET)
 endmacro (SetCXX)
 
 # settings for Fortran
 macro (SetFortran)
   enable_language (Fortran)
   set (CMAKE_Fortran_FLAGS "-cpp")
+  if (QUIET)
+    set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -w")
+  endif (QUIET)
 endmacro (SetFortran)
 
 # shortcut to call all the above macros
@@ -18,3 +24,11 @@ macro (SetAll)
   SetCXX ()
   SetFortran ()
 endmacro (SetAll)
+
+
+# generic settings
+option(QUIET "Suppress warnings." OFF)
+
+if (QUIET)
+  message (STATUS "Selected quiet build, warnings will be suppressed.")
+endif (QUIET)
