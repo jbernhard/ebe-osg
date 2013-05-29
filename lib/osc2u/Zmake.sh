dcb56d07779f32128d775205699c26e16756759c
Jonah Bernhard <jeb65@engage-submit3.renci.org>
2013-05-29 14:22:21 -0400

    Remove non-CMake build files.

--- lib/osc2u/Zmake.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ../set-for
-
-$FC osc2u.f pdg2ityp.f vni_procev.f blockres.f dectim.f gnuranf.f -o osc2u $FFLAGS
--- lib/superMC/Zmake.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ../set-cpp
-
-$CC main.cpp Bases.cpp MCnucl.cpp GlueDensity.cpp MakeDensity.cpp KLNModel.cpp OverLap.cpp Largex.cpp Regge96.cpp rcBKfunc.cpp MathBasics.cpp ParameterReader.cpp arsenal.cpp EOS.cpp GaussianNucleonsCal.cpp NBD.cpp RandomVariable.cpp TableFunction.cpp Table.cpp -o superMC $CFLAGS
