362c07a0391d3dddd03ebba9568161064989be5b
Jonah Bernhard <jeb65@grads-ad>
2013-06-04 15:35:07 -0400

    Convert R LHS script to unix file format [so git thinks every line has changed].  Clean up some code.  Update parameter ranges.

--- etc/lhs-design.r
+++ etc/lhs-design.r
@@ -1,6 +1,6 @@
 
 ### Generating a MmLHS design with rows of decreasing distances
-### ... a MmLHS with rows reordered according to a Coffeehouse rule 
+### ... a MmLHS with rows reordered according to a Coffeehouse rule
 
 library(lhs)                     ### for Latin Hypercube stuff
 
@@ -31,19 +31,19 @@ CHD <- function(X.all,N){        ### N.all >= N >= 3
    dist.mat <- as.matrix( dist(X.all) )
    dist.mat.temp <- dist.mat
    dist.mat.temp[row(dist.mat)>=col(dist.mat)] <- 0
-   ind <- as.vector( which(dist.mat.temp==max(dist.mat.temp), 
+   ind <- as.vector( which(dist.mat.temp==max(dist.mat.temp),
                            arr.ind=TRUE) )
    X[1:2,] <- X.all[ind,]
 
-   for(n in 3:N){ 
+   for(n in 3:N){
       dist.mat.temp <- dist.mat
       dist.mat.temp[row(dist.mat) == col(dist.mat)] <- Inf
       dist.mat.temp[ind,ind] <- Inf
       ind.compl <- setdiff(1:N.all,ind)
       dist.mat.temp[ind.compl,ind.compl] <- Inf
                       ### minimimum distances to current design, X
-      col.mins <- apply(dist.mat.temp,2,'min')   
-      col.mins[ind] <- 0  
+      col.mins <- apply(dist.mat.temp,2,'min')
+      col.mins[ind] <- 0
       ind.temp <- which( col.mins==max(col.mins) )
       X <- rbind( X, X.all[ind.temp,] )
       ind <- c(ind,ind.temp)
@@ -53,7 +53,7 @@ CHD <- function(X.all,N){        ### N.all >= N >= 3
 #D <- 2                          ### to see what 'CHD' does
 #N.all <- 1000
 #X.all <- matrix( runif(N.all*D), N.all,D )
-#X.all[,2] <- X.all[,1]^2 + rnorm(N.all, 0,0.15) 
+#X.all[,2] <- X.all[,1]^2 + rnorm(N.all, 0,0.15)
 #X <- CHD(X.all,N=8)
 #plot(X.all)  ;  points(X, col=2, pch=19)
 
@@ -68,7 +68,7 @@ CHD <- function(X.all,N){        ### N.all >= N >= 3
 #X <- scaleto.mM(X=X.order, M=M)
 #
 #par(mfrow=c(1,3))
-#plot(X.scale, pch=as.character(1:9))  
+#plot(X.scale, pch=as.character(1:9))
 #plot(X.order, pch=as.character(1:9))
 #plot(X, pch=as.character(1:9))
 
@@ -78,35 +78,25 @@ CHD <- function(X.all,N){        ### N.all >= N >= 3
 
 
 
-
-
-
 ########## Glauber IC model
-# Fixing the EOS to the default value: 7 (s95p-v0-PCE) # Fixing the decoupling energy (Edec) to the defaul value: 0.502
 
 set.seed(314159)
 
 D <- 5
 #N <- 20*D
 N <- 256
-            ### IC:'FinalFactor', IC:'alpha', tau_0, eta/s, VisBeta
+
 X.scale <- maximinLHS(n=N, k=D)
 X.order <- CHD(X.all=X.scale, N=N)
-M <- matrix( c(40,70,  0.10,0.16,  
+M <- matrix( c(20,60,  0.05,0.3,
                0.2,1.0,  0.0,0.3,  0.2,1.1),   nrow=2 )
 X <- scaleto.mM(X=X.order, M=M)
 X <- data.frame(X)
-names(X) <- c("IC.FinalFactor","IC.alpha","tau0","eta.s","VisBeta") 
-#plot(X)
+names(X) <- c("superMC.finalFactor","superMC.alpha","VISHNew.T0","VISHNew.ViscousC","VISHNew.VisBeta")
 
-
-
-write.table( x=X, file=paste0('lhs-design-glb-',N,'.dat'), 
-             append=FALSE, sep=" ", 
+write.table( x=X, file=paste0('lhs-design-glb-',N,'.dat'),
+             append=FALSE, sep=" ",
              row.names=FALSE, col.names = TRUE )
-
-
-
 
 
 
@@ -115,30 +105,20 @@ write.table( x=X, file=paste0('lhs-design-glb-',N,'.dat'),
 
 ########## MC-KLN IC model
 #
-#
-
 #set.seed(314159)
 #
 #D <- 5
-#N <- 20*D
-#            ### IC:'FinalFactor', IC:'lamba', tao_0, eta/s, VisBeta
+##N <- 20*D
+#N <- 256
+#
 #X.scaled <- maximinLHS(n=N, k=D)
 #X.order <- CHD(X.all=X.scale, N=N)
-#M <- matrix( c(5,20,  0.10,0.25,  
+#M <- matrix( c(5,11,  0.1,0.3,
 #               0.2,1.0,  0.0,0.3,  0.2,1.1),   nrow=2 )
 #X <- scaleto.mM(X=X.order, M=M)
 #X <- data.frame(X)
-#names(X) <- c("IC.FinalFactor","IC.lambda","tau0","eta.s","VisBeta") 
-#plot(X)
-
-
-
-#write.table( x=X, file="design-MC_KLN-100.txt", 
-#             append=FALSE, sep=" ", 
+#names(X) <- c("superMC.finalFactor","superMC.lambda","VISHNew.T0","VISHNew.ViscousC","VISHNew.VisBeta")
+#
+#write.table( x=X, file=paste0('lhs-design-kln-',N,'.dat'),
+#             append=FALSE, sep=" ",
 #             row.names=FALSE, col.names = TRUE )
-
-
-
-
-
-
